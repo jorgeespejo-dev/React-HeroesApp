4b9edb44ac1d4c18a833aad6fc570ec78f540a6d
Jespejo <jorgeespejo.dev@gmail.com>
2020-12-14 17:23:05 -0300

    list heroes && selectors, getHeroeByPublisher, id

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { HeroList } from '../heroes/HeroList'
 
 export const MarvelScreen = () => {
     return (
         <div>
             <h1>Marven Screen</h1>
+            <hr />
+            <HeroList publisher="Marvel Comics" />
         </div>
     )
 }
